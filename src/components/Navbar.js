18a05bba82db230799c2cb02bb55120f409892c4
leandroc0rreia <leandrocorreia.geral@gmail.com>
2022-07-21 16:05:33 +0100

    Navbar without linking

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,32 +5,29 @@ function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
-      <nav className="bg-white">
+      <nav className="bg-gray-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
-            <div className="flex items-center">
+            <div className="container flex flex-wrap justify-between items-center mx-auto">
               <div className="flex-shrink-0">
                 <a class="flex items-center">
-                    <img src="/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="LECO logo" />
+                    <img src="../logo.png" class="mr-3 h-6 sm:h-9" alt="LECO logo" />
                     <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-black">Leandro Correia</span>
                 </a>
               </div>
               <div className="hidden md:block">
                 <div className="ml-10 flex items-baseline space-x-4">
-                  <a href="#" className="text-blue-500 hover:bg-gray-50 text-black hover:text-blue-700 px-3 py-2 rounded-md text-sm font-medium">
-                    Dashboard
+                  <a href="#" className="text-blue-500 hover:bg-blue-50 text-black hover:text-blue-800 px-3 py-2 rounded-md text-sm font-medium">
+                    Homepage
                   </a>
-                  <a href="#" className="text-blue-500 hover:bg-gray-50 text-black hover:text-blue-700 px-3 py-2 rounded-md text-sm font-medium">
-                    Team
+                  <a href="#" className="text-blue-500 hover:bg-blue-50 text-black hover:text-blue-800 px-3 py-2 rounded-md text-sm font-medium">
+                    Sobre
                   </a>
-                  <a href="#" className="text-blue-500 hover:bg-gray-50 text-black hover:text-blue-700 px-3 py-2 rounded-md text-sm font-medium">
-                    Projects
+                  <a href="#" className="text-blue-500 hover:bg-blue-50 text-black hover:text-blue-800 px-3 py-2 rounded-md text-sm font-medium">
+                    Projectos
                   </a>
-                  <a href="#" className="text-blue-500 hover:bg-gray-50 text-black hover:text-blue-700 px-3 py-2 rounded-md text-sm font-medium">
-                    Calendar
-                  </a>
-                  <a href="#" className="text-blue-500 hover:bg-gray-50 text-black hover:text-blue-700 px-3 py-2 rounded-md text-sm font-medium">
-                    Reports
+                  <a href="#" className="text-blue-500 hover:bg-blue-50 text-black hover:text-blue-800 px-3 py-2 rounded-md text-sm font-medium">
+                    Contacto
                   </a>
                 </div>
               </div>
@@ -39,7 +36,7 @@ function Navbar() {
               <button
                 onClick={() => setIsOpen(!isOpen)}
                 type="button"
-                className="bg-gray-50 inline-flex items-center justify-center p-2 rounded-md text-black hover:text-black hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-white"
+                className="bg-blue-50 inline-flex items-center justify-center p-2 rounded-md text-black hover:text-black hover:bg-blue-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-blue-50 focus:ring-gray-50"
                 aria-controls="mobile-menu"
                 aria-expanded="false"
               >
@@ -93,21 +90,18 @@ function Navbar() {
           {(ref) => (
             <div className="md:hidden" id="mobile-menu">
               <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                <a href="#" className="text-blue-500 hover:bg-gray-50 text-black hover:text-blue-700 block px-3 py-2 rounded-md text-base font-medium">
-                  Dashboard
+                <a href="#" className="text-blue-500 hover:bg-blue-50 text-black hover:text-blue-700 block px-3 py-2 rounded-md text-base font-medium">
+                  Homepage
                 </a>
-                <a href="#" className="text-blue-500 hover:bg-gray-50 text-black hover:text-blue-700 block px-3 py-2 rounded-md text-base font-medium">
-                  Team
+                <a href="#" className="text-blue-500 hover:bg-blue-50 text-black hover:text-blue-700 block px-3 py-2 rounded-md text-base font-medium">
+                  Sobre
                 </a>
 
-                <a href="#" className="text-blue-500 hover:bg-gray-50 text-black hover:text-blue-700 block px-3 py-2 rounded-md text-base font-medium">
-                  Projects
+                <a href="#" className="text-blue-500 hover:bg-blue-50 text-black hover:text-blue-700 block px-3 py-2 rounded-md text-base font-medium">
+                  Projectos
                 </a>
-                <a href="#" className="text-blue-500 hover:bg-gray-50 text-black hover:text-blue-700 block px-3 py-2 rounded-md text-base font-medium">
-                  Calendar
-                </a>
-                <a href="#" className="text-blue-500 hover:bg-gray-50 text-black hover:text-blue-700 block px-3 py-2 rounded-md text-base font-medium">
-                  Reports
+                <a href="#" className="text-blue-500 hover:bg-blue-50 text-black hover:text-blue-700 block px-3 py-2 rounded-md text-base font-medium">
+                  Contacto
                 </a>
               </div>
             </div>
