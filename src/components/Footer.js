daa812e681e75d65a2e4a7b5870e393296a5ce5d
leandroc0rreia <leandrocorreia.geral@gmail.com>
2022-07-23 21:26:07 +0100

    some updates on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,25 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>
-      <div className="flex justify-center items-center h-16 bg-bla text-white">
-        <p>Copyright © 2022 LECO All rights reserved.</p>
+      <div className="mt-auto py-7 text-center bg-blue-50 mx-auto">
+        <div className="container mx-auto lg:px-16 md:px-14 sm:px-12 px-10">
+          <div className="display-4 pb-4 text-center justify-center lg:text-5xl md:text-4xl sm:text-3xl text-2xl font-medium">
+            Entra em contacto!
+          </div>
+          <div>
+            <p className='lead text-center pb-3 justify-center mx-auto lg:text-xl md:text-lg sm:text-lg text-md font-light'>
+              De momento estou à procura de um part-time em Desenvolvimento Web (Fullstack ou Front/Back-end) ou Desenvolvimento Mobile, caso saiba alguma oportunidade, se está com alguma questão ou se só quer dizer um olá, esteja à vontade para me contactar em,&nbsp;
+              <a href='mailto:geral@leandroc0rreia.com' className='text-blue-600'>
+                geral@leandroc0rreia.com
+              </a>
+              .
+            </p>
+            <p>
+              <small class="text-muted text-gray-600">O projeto é open-source. Esteja à vontade para contribuir e fazer a sua própria versão.</small>
+            </p>
+          </div>
+        </div>
       </div>
-    </div>
   )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
-      <nav className="bg-white shadow-sm">
+      <nav className="bg-white shadow-sm shadow-blue-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="container flex flex-wrap justify-between items-center mx-auto">
@@ -88,9 +88,8 @@ function Navbar() {
           leave="transition ease-in duration-75 transform"
           leaveFrom="opacity-100 scale-100"
           leaveTo="opacity-0 scale-95">
-          {(ref) => (
-            <div className="md:hidden" id="mobile-menu">
-              <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+          {<div className="md:hidden" id="mobile-menu">
+              <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
                 <Link to="/" className="text-blue-500 hover:bg-blue-50 text-black hover:text-blue-700 block px-3 py-2 rounded-md text-base font-medium">
                   Home
                 </Link>
@@ -104,8 +103,7 @@ function Navbar() {
                   Contacto
                 </Link>
               </div>
-            </div>
-          )}
+            </div>}
         </Transition>
       </nav>
     </div>
