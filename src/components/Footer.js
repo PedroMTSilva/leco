770107b3536b61b64cbe349781c30d04639a0768
leandroc0rreia <leandrocorreia.geral@gmail.com>
2022-07-24 15:15:41 +0100

    starting darkmode

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,16 +7,16 @@ function Footer() {
           <div className="display-4 pb-4 text-center justify-center lg:text-5xl md:text-4xl sm:text-3xl text-2xl font-medium">
             Entra em contacto!
           </div>
-          <div>
-            <p className='lead text-center pb-3 justify-center mx-auto lg:text-xl md:text-lg sm:text-lg text-md font-light'>
-              De momento estou à procura de um part-time em Desenvolvimento Web (Fullstack ou Front/Back-end) ou Desenvolvimento Mobile, caso saiba alguma oportunidade, se está com alguma questão ou se só quer dizer um olá, esteja à vontade para me contactar em,&nbsp;
+          <div className='lg:mx-12'>
+            <p className='lead pb-3 justify-center mx-auto lg:text-xl md:text-lg sm:text-lg text-md font-light text-justify'>
+              De momento estou à procura de um part-time em Desenvolvimento Web (Front/Back-end) ou Desenvolvimento Mobile, caso esteja com interesse, alguma questão ou se só quer dizer um olá, esteja à vontade para me contactar em,&nbsp;
               <a href='mailto:geral@leandroc0rreia.com' className='text-blue-600'>
                 geral@leandroc0rreia.com
               </a>
               .
             </p>
             <p>
-              <small class="text-muted text-gray-600">O projeto é open-source. Esteja à vontade para contribuir e fazer a sua própria versão.</small>
+              <small className="text-muted text-gray-600">O projeto é open-source. Esteja à vontade para contribuir ou fazer a sua própria versão.</small>
             </p>
           </div>
         </div>
