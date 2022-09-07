067de18d21d3835e9151811e16e7b2b5a8c1cff3
leandroc0rreia <leandrocorreia.geral@gmail.com>
2022-09-07 17:29:45 +0100

    test

--- src/services/fetchRepositories.js
+++ src/services/fetchRepositories.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import data from '../config.json'
 
-const API = `https://api.github.com/users/${data.username}/repos`;
+const API = `https://api.github.com/users/${data.username}/repos?sort=updated&direction=desc`;
 
 const useFetchRepositories = () => {
     const [repositories, setRepositories] = useState([]);
